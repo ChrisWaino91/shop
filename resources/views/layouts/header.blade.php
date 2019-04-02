82ac810541785f24a1452fdeccf085b5f252dfb2
ChrisWaino91 <christopherwainwright91@gmail.com>
2019-04-02 14:09:07 +0100

    Use dynamic css ref from views to correct 404s

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-  <link href="/css/app.css" rel="stylesheet"/>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
 
   <nav class="navbar navbar-expand">
     <a class="navbar-brand" href="/shop">SHOP</a>
